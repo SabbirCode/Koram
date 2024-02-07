448f9e12e46c3eb3ee62dee832d8e988233a6e85
SabbirCode <126693471+SabbirCode@users.noreply.github.com>
2024-02-08 02:44:49 +0600

    Update js2.js

--- js/js2.js
+++ js/js2.js
@@ -1,1 +1,1 @@
-document.write('<img src="https://www.freevisitorcounters.com/en/counter/render/1139225/t/0" border="0" class="center">');
+document.write('<img src="https://www.freevisitorcounters.com/en/counter/render/1139225/t/0" border="0" class="counterimg">');
